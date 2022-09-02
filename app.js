f53d17d3118fc2cd550ca65164e009ef07bcb853
md-riaj11 <contact.riaj@gmail.com>
2022-09-02 17:33:53 +0600

    search add

--- app.js
+++ app.js
@@ -24,8 +24,6 @@ loadGenre();
 
 const loadMovieList = async () => {
     let { results } = await loadData("discover/movie",)
-    console.log(results);
-
 
     const movieList = document.getElementById("movie-list");
     results.forEach(movie => {
@@ -61,4 +59,44 @@ const loadMovieList = async () => {
     })
 
 }
+
 loadMovieList()
+document.getElementById("searchbtn").addEventListener('click', async () => {
+    const searchText = document.getElementById("searchbox").value;
+    let { results } = await loadData("search/movie", `query=${searchText}&page=1&include_adult=false`)
+
+    document.getElementById("movie-list").innerHTML = "";
+
+    const movieList = document.getElementById("movie-list");
+    results.forEach(movie => {
+        const backMovieImage = imageFirst + movie.backdrop_path;
+        const poster = imageFirst + movie.poster_path;
+        const overview = movie.overview
+        const movieDiv = document.createElement("div")
+        movieDiv.innerHTML = `
+                            <div class="movie_card">
+                    <div class="info_section">
+                        <div class="movie_header">
+                            <img class="locandina"
+                                src="${poster}" />
+                            <h1>${movie.title}</h1>
+                            <h4> ${movie.release_date}</h4>
+                            <span class="minutes">117 min</span>
+                            <p class="type">Action, Crime, Fantasy</p>
+                        </div>
+                        <div class="movie_desc">
+                            <p class="text">
+                                 ${overview.split(" ").splice(0, 30).join(" ")}
+                            </p>
+                        </div>
+
+                    </div>
+                    <div class="blur_back"
+                        style='background: url("${backMovieImage}"); background-position: right center !important; background-size: cover;'>
+                    </div>
+                </div>
+
+        `;
+        movieList.appendChild(movieDiv);
+    })
+})
